7227ae803410723eb7e7fecf8948ccb2d2aedc8f
Sibusiso Bhengu <Sbhengu@nmmu.ac.za>
2022-07-30 04:18:24 +0200

    Add project files.

---
+++ Ops/Models/OperationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ops.Models
+{
+    public class OperationModel
+    {
+        public int OperationID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int OrderInWhichToPerform { get; set; }
+        public byte[] ImageData { get; set; }
+        public DeviceModel Device { get; set; }
+    }
+}
